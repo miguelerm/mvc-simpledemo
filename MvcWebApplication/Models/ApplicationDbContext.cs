0337deed182fb927d6b5c8f019b4866bfd14ac46
Miguel Román <miguelerm@gmail.com>
2014-10-28 11:42:45 -0600

    Se creó el modelo Usuario y el ApplicacionDbContext.

---
+++ MvcWebApplication/Models/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MvcWebApplication.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Usuario> Usuarios { get; set; }
+    }
+}
---
+++ MvcWebApplication/Models/Usuario.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace MvcWebApplication.Models
+{
+    [Table("Usuarios")]
+    public class Usuario
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public DateTime FechaCreacion { get; set; }
+
+        [Required]
+        [MinLength(3)]
+        [StringLength(20)]
+        public string Login { get; set; }
+
+        [Required]
+        [MinLength(3)]
+        [StringLength(200)]
+        public string Correo { get; set; }
+
+        [Required]
+        [MinLength(3)]
+        [StringLength(200)]
+        public string Nombre { get; set; }
+
+        [Required]
+        [MinLength(3)]
+        [StringLength(200)]
+        [DataType(DataType.Password)]
+        public string Clave { get; set; }
+    }
+}
