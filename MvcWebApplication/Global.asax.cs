e8415be850e14d1b6598026b2a781de8e7a2dec1
Miguel Román <miguelerm@gmail.com>
2014-10-29 16:01:21 -0600

    Se habilita que actualice la base de datos a la ultima versión automáticamente.

--- MvcWebApplication/Global.asax.cs
+++ MvcWebApplication/Global.asax.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using MvcWebApplication.Migrations;
+using MvcWebApplication.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -12,6 +15,7 @@ namespace MvcWebApplication
     {
         protected void Application_Start()
         {
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<ApplicationDbContext, Configuration>()); 
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
